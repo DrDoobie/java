c68345ea9c01413af7b1eb5a62dcc9185cc31ee9
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-05 10:33:03 +0200

    Random number generator

---
+++ eclipse-workspace/intro/src/intro/generator.java
@@ -0,0 +1,19 @@
+package intro;
+import java.util.Random;
+
+public class generator {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	//Generate a random number
+	private static void GenerateRandom (float max) {
+		System.out.println(Math.floor((Math.random() * max) + 1));
+	}
+	
+	//Generate a random sentence.
+	private static void GenerateSentence () {
+		
+	}
+}
