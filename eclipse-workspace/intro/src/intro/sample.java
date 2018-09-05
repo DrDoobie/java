ad46168576658840534b5eb58e0667c8050bde2b
DrDoobie <34827182+DrDoobie@users.noreply.github.com>
2018-09-05 09:28:51 +0200

    Sample class

---
+++ eclipse-workspace/intro/src/intro/sample.java
@@ -0,0 +1,10 @@
+package intro;
+
+public class sample {
+//Complete beginner to Java, this is mainly for playing around/note taking
+
+	public static void main(String[] args) {
+		//This is how you print code to the console.
+		System.out.println("Hello World");
+	}
+}
